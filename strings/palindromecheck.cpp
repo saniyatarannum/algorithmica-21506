658df3efc87ee6868a6abdf0082d3b42d528ca8e
saniyatarannum <21wh5a0506@bvrithyderabad.edu.in>
2022-12-26 22:20:02 +0530

    done

---
+++ strings/palindromecheck.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char string[20];
+	int i, length;
+	int flag = 0;
+	printf("Enter a string:");
+	scanf("%s", string);
+	length = strlen(string);
+	for(i=0;i < length ;i++)
+	{
+		if(string[i] != string[length-i-1])
+		{
+			flag = 1;
+			break;
+        }  
+    }
+    if (flag) 
+	{
+		printf("%s is not palindrome", string);
+	}
+	else 
+	{
+		printf("%s is  palindrome", string);
+    }
+    return 0;
+}
